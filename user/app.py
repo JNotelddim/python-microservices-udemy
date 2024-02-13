aab67465fe39977abadb5b70b147dca5555588c0
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-12 16:01:49 -0800

    Set up initial migration and use User db in `/all` query

--- user/app.py
+++ user/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
 
 from routes import user_blueprint
 import models
@@ -7,8 +9,15 @@ app = Flask(__name__)
 
 # Of course we'd never store the secret key in the code :eyes
 app.config['SECRET_KEY'] = 'nBwg9uSus8m6kqApYqHUOQ'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database/user.db'
+
+file_path = os.path.abspath(os.getcwd())+"/database/user.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+
 models.init_app(app)
+
 app.register_blueprint(user_blueprint)
 
-app.run()
+migrate = Migrate(app, models.db)
+
+if __name__ == "__main__":
+    app.run()
