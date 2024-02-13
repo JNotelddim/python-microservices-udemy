fc7ba154ba2982d7da28620296d9e1d544328bf4
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-12 16:12:39 -0800

    Handle creating users

--- user/routes.py
+++ user/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 user_blueprint = Blueprint('user_api_routes', __name__, url_prefix='/api/user')
 
@@ -13,4 +14,21 @@ def get_all_users():
         'result': result
     }
 
+    return jsonify(response)
+
+@user_blueprint.route('/create', methods=['POST'])
+def create_user():
+    try:
+        new_user = User()
+        new_user.username = request.form["username"]
+        new_user.password = generate_password_hash(request.form["password"]) #, method="sha256")
+        new_user.is_admin = True # only cause this is dev.
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        response = {'message': "User created.", 'result': new_user.serialize()}
+    except Exception as e:
+        print(e)
+        response = { 'message': "Error creating user."}
     return jsonify(response)
