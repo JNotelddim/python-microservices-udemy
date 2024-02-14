d5a31874b98fa207fd533e00ef6215ab33d272e4
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-14 15:39:00 -0800

    Create skeleton endpoints for book routes

--- book/app.py
+++ book/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 import os
 
+from routes import book_blueprint
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "WtlLAh2AZllfQkitoY6KqA"
 file_path = os.path.abspath(os.getcwd())+"/database/book.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+
+app.register_blueprint(book_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
