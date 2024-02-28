6f4a380831d50c272d4977b0f5e8355a8c6fed5d
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-27 16:36:09 -0800

    Implement `/add-item` order endpoint

--- order/routes.py
+++ order/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 import requests
 
-from models import Order
+from models import Order, OrderItem, db
 
 order_blueprint = Blueprint('order_api_route', __name__, url_prefix="/api/order")
 
@@ -31,7 +31,43 @@ def get_all_orders():
 
 @order_blueprint.route('/add-item', methods=['POST'])
 def add_order_item():
-    return "adding item"
+    api_key = request.headers.get('Authorization')
+    if not api_key:
+        return make_response(jsonify({ 'message': 'Not logged in.'}), 401)
+
+    response = get_user(api_key=api_key)
+    if not response.get('result'):
+        return make_response(jsonify({ 'message': 'Not logged in.'}), 401)
+
+    user = response.get('result')
+    book_id = int(request.form['book_id'])
+    quantity = int(request.form['quantity'])
+    user_id = user['id']
+
+    open_order = Order.query.filter_by(user_id=user_id, is_open=1).first()
+
+    if not open_order:
+        open_order = Order()
+        open_order.is_open = True
+        open_order.user_id = user_id
+
+        order_item = OrderItem(book_id=book_id, quantity=quantity)
+        open_order.order_items.append(order_item)
+    else:
+        found = False
+        for item in open_order.order_items:
+            if item.book_id == book_id:
+                item.quantity += quantity
+            found = True
+
+        if not found:
+            order_item = OrderItem(book_id=book_id, quantity=quantity)
+            open_order.order_items.append(order_item)
+
+    db.session.add(open_order)
+    db.session.commit()
+
+    return jsonify({ 'result': open_order.serialize()})
 
 @order_blueprint.route('/checkout', methods=['POST'])
 def checkout():
