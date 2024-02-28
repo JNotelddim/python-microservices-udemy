8d26a731124b2f1c916fb193397bdc49044d9d20
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-27 16:26:17 -0800

    Implement `/all` orders endpoint w/ connection to user service

--- order/routes.py
+++ order/routes.py
@@ -1,9 +1,23 @@
 from flask import Blueprint, request, jsonify, make_response
 import requests
 
+from models import Order
+
 order_blueprint = Blueprint('order_api_route', __name__, url_prefix="/api/order")
 
 USER_API_URL = 'http://127.0.0.1:5001/user/api'
+
+def get_user(api_key):
+    headers = {
+        'Authorization': api_key,
+    }
+
+    response = requests.get(USER_API_URL, headers=headers)
+    if response.status_code != 200:
+        return { 'message': 'Not Authorized'}
+
+    user = response.json()
+    return user
 
 @order_blueprint.route('/', methods=['GET'])
 def get_open_order():
@@ -11,7 +25,9 @@ def get_open_order():
 
 @order_blueprint.route('/all', methods=['GET'])
 def get_all_orders():
-    return "all orders"
+    all_orders = Order.query.all()
+    result = [order.serialize() for order in all_orders]
+    return jsonify(result)
 
 @order_blueprint.route('/add-item', methods=['POST'])
 def add_order_item():
