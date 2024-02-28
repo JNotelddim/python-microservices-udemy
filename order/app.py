a75eef5edc9e174cffddff0183e3148e820c5ce5
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-27 16:21:17 -0800

    Set up models and init db for order service

--- order/app.py
+++ order/app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
 
 from routes import order_blueprint
+from models import db, init_app
 
 app = Flask(__name__)
 
@@ -10,7 +12,8 @@ app.config['SECRET_KEY'] = 'nBwg9uSus8m6kqApYqHUOQ'
 file_path = os.path.abspath(os.getcwd())+"/database/order.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 
-# models.init_app(app)
+init_app(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(order_blueprint)
 
