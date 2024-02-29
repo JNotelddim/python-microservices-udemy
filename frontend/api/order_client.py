d14e4539b130d9f9005ccf60b087725e06ecb0d9
JNotelddim <JNotelddim@users.noreply.github.com>
2024-02-28 16:59:19 -0800

    Create book and order client files in the frontend

---
+++ frontend/api/order_client.py
@@ -0,0 +1,39 @@
+import requests
+from flask import session
+
+from . import ORDER_API_URL
+
+class OrderClient:
+    @staticmethod
+    def get_order():
+        headers = {'Authorization': session['user_api_key']}
+
+        response = requests.get(ORDER_API_URL + '/api/order', headers=headers)
+        return response.json()
+
+    @staticmethod
+    def add_to_cart(book_id, quantity=1):
+        payload = {
+            'book_id': book_id,
+            'quantity': quantity,
+        }
+
+        headers = { 'Authorization': session['user_api_key']}
+        response = requests.post(ORDER_API_URL+'/api/order/add-item',
+                                 headers=headers,
+                                 data=payload)
+        return response.json()
+
+    @staticmethod
+    def checkout():
+        headers = { 'Authorization': session['user_api_key']}
+        response = requests.post(ORDER_API_URL+'/api/order/checkout', headers=headers)
+
+        return response.json()
+
+    @staticmethod
+    def get_order_from_session():
+        default_order = {
+            'items': {}
+        }
+        return session.get('order', default_order)
